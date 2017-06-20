6c6eb8ce734a9ffe9f2ae23a6685f9bcb52b373d
Gowtham Kudupudi <gowtham@ferryfair.com>
2017-06-20 10:34:45 +0530

    Initial Commit

---
+++ MetalDeferredLightingMacOS/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MetalDeferredLightingMacOS
+//
+//  Created by Gowtham Kudupudi on 19/06/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
